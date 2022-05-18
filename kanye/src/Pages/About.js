aacc3f1c86b38a759e97fceaae4ed3a325408027
Andrew Makris <amakri386@west-mec.org>
2022-05-18 12:07:32 -0700

    Push it real good baby

--- kanye/src/Pages/About.js
+++ kanye/src/Pages/About.js
@@ -1,49 +1,60 @@
-import React from 'react'
+import {React,useState} from 'react'
+import Relationship from '../Util/Relationships'
 
 const About = () => {
+    const [girl,setGirl] = useState(Relationship)
     return (
-        <div>
+        <div className="about">
             <div className="about-container">
                 <h3>Who is Kanye West?</h3>
                 <hr />
                 <p>Ye (/jeɪ/ YAY; born Kanye Omari West; June 8, 1977), commonly known as Kanye West[c] (/ˈkɑːnjeɪ/ KAHN-yay), is an American rapper, record producer, entrepreneur and fashion designer. He is widely regarded as one of the greatest and most influential hip hop musicians of all time, as well as one of the greatest musicians of his generation. Born in Atlanta and raised in Chicago, West gained recognition as a producer for Roc-A-Fella Records in the early 2000s, producing singles for several artists and developing the "chipmunk soul" sampling style. Intent on pursuing a solo career as a rapper, he released his debut studio album, The College Dropout (2004), to critical and commercial success and subsequently founded the record label GOOD Music.West explored diverse musical elements such as orchestral arrangements, synthesizers, and autotune on the albums Late Registration (2005), Graduation (2007), and 808s & Heartbreak (2008). Drawing inspiration from maximalism and minimalism respectively, his fifth album My Beautiful Dark Twisted Fantasy (2010) and sixth album Yeezus (2013) were also released to critical and commercial success. West further diversified his musical styles on The Life of Pablo (2016) and Ye (2018), and explored Christian and gospel music on Jesus Is King (2019). After a series of delays, his highly anticipated tenth album Donda (2021) was released to continued commercial success, but to mixed critical reception. He released its sequel, Donda 2 (2022), a few months later, exclusively through his tie-in audio speaker service. West's discography also includes the full-length collaborative albums Watch the Throne (2011) with Jay-Z and Kids See Ghosts (2018) with Kid Cudi.West's outspoken views and life outside of music have received significant media coverage. He has been a frequent source of controversy for his conduct on social media, award shows, and public settings; as well as his comments on the music and fashion industries, U.S. politics, race, and slavery. His Christian faith, estranged marriage to Kim Kardashian, and mental health have also been sources of further media attention. As a fashion designer, he has collaborated with Nike, Louis Vuitton, The Gap, and A.P.C. on clothing and footwear, and leads the Yeezy collaboration with Adidas. He is also the founder and head of the creative content company Donda. In 2020, West launched an unsuccessful independent presidential campaign that primarily advocated for a consistent life ethic.One of the world's best-selling music artists, with over 160 million records sold, West has won 24 Grammy Awards, the joint tenth-most of all time, and the joint-most Grammy awards of any rapper along with Jay-Z. He is frequently lauded as one of hip hop's greatest artists, with his music among the most acclaimed.[12][13] Among his other awards are the Billboard Artist Achievement Award, a joint-record three Brit Awards for Best International Male Solo Artist and the Michael Jackson Video Vanguard Award.[14] Six of West's albums were included on Rolling Stone's 2020 500 Greatest Albums of All Time list with the same publication naming him one of the 100 Greatest Songwriters of All Time. He holds the joint record (with Bob Dylan) for most albums (4) topping the annual Pazz & Jop critic poll, and has the 5th most appearances on the Billboard Hot 100 (133 entries). Time magazine named him one of the 100 most influential people in the world in 2005 and 2015. Forbes estimates his net worth at $2 billion as of 2022, making him the wealthiest musician in the world</p>
             </div>
+            
+            <h1>Kanye and his friends</h1>
+            <hr/>
             <div className="colab-container">
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/474x/69/fb/87/69fb876d30e77728e474ec982ef28a5d--good-music-artists-jayz.jpg" alt="" />
-                    <h2>Kanye and Jay-Z</h2>
                 </div>
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/originals/8e/31/be/8e31be1c260cc64ba5140818404fb480.jpg" alt="" />
-                    <h2>Kanye and Kid Cudi</h2>
                 </div>
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/736x/28/4b/6f/284b6f58e1ec004c7255304969938da2.jpg" alt="" />
-                    <h2>Kanye and Travis Scott </h2>
-                </div>
-                <div className="colab-card">
-                    <img src="https://i.pinimg.com/736x/a2/a2/b0/a2a2b046240b2e27d51f18a7956d6f47.jpg" alt="" />
-                    <h2>Kanye and Rihanna</h2>
                 </div>
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/originals/c6/cf/92/c6cf92879c8fd0237baf8afa93c6e7d0.png" alt="" />
-                    <h2>Kanye and Frank Ocean</h2>
                 </div>
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/736x/ae/82/e1/ae82e1fa03444d14252ca85f8abef48e.jpg" alt="" />
-                    <h2>Kanye and Drake</h2>
                 </div>
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/originals/2e/48/06/2e48063a96dcb28e53c49e25d56cb983.jpg" alt="" />
-                    <h2>Kanye and Kendrick</h2>
                 </div>
                 <div className="colab-card">
                     <img src="https://i.pinimg.com/originals/09/32/61/09326117dd639b122aca149bd53c52ed.png" alt="" />
-                    <h2>Kanye and Chance the Rapper</h2>
                 </div>
+                <div className="colab-card">
+                    <img src="https://securea.mlb.com/assets/images/3/8/0/67725380/cuts/6eb6e8c494bd11e3a8551252a0dde04d_8_aq3il50w_x5fjftyn.jpg" alt="" />
+                </div>
+            </div>
+            
+            <div className="kanye-relationships">
+                {girl.map((love)=>{
+                    const {id,Name,Time,Img} = love;
+                    return(
+                        <div key={id}>
+                            <img src={Img} alt={Name}/>
+                            <h2>{Name}</h2>
+                            
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
 }
+
 
 export default About
---
+++ kanye/src/Util/Relationships.js
@@ -0,0 +1,64 @@
+const girls = [
+    {
+        id:1,
+        Name:'Sumeke Rainey',
+        Time:'Late 90s',
+        Img:'https://media1.popsugar-assets.com/files/thumbor/QjVVVJF7fYOW3zNAEtDCBuPAmWk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/03/30/900/n/1922398/040421101eda4659_GettyImages-83717638/i/Sumeke-Rainey.jpg',
+    },
+    {
+        id:2,
+        Name:'Brooke Crittendon',
+        Time:'2004',
+        Img:'https://imgs.capitalxtra.com/images/211298?crop=16_9&width=660&relax=1&signature=q0jTiuoBs_JW7ONu__fEpooNdds=',
+    },
+    {
+        id:3,
+        Name:'Alexis Phifer',
+        Time:'2002-2008',
+        Img:'https://imgs.capitalxtra.com/images/211296?crop=16_9&width=660&relax=1&signature=1ZHk9bmh3Ax3EloEmZBq1cjOhuA=',
+    },
+    {
+        id:4,
+        Name:'Sessilee Lopez',
+        Time:'2008',
+        Img:'https://imgs.capitalxtra.com/images/211294?crop=16_9&width=660&relax=1&signature=5_sWL3tHT9Gxg6yayjAwa_cuLfc=',
+    },
+    {
+        id:5,
+        Name:'Amber Rose',
+        Time:'2008-2010',
+        Img:'https://imgs.capitalxtra.com/images/211282?crop=16_9&width=660&relax=1&signature=VPLeWKwksq1HOYQhOiWumPiunmU=',
+    },
+    {
+        id:6,
+        Name:'Selita Ebanks',
+        Time:'2010',
+        Img:'https://imgs.capitalxtra.com/images/211280?crop=16_9&width=660&relax=1&signature=Q7Ku5Qo5vuUX6EGMfZQc1q9xPM4=',
+    },
+    {
+        id:7,
+        Name:'Chanel Iman',
+        Time:'2010',
+        Img:'https://imgs.capitalxtra.com/images/211263?crop=16_9&width=660&relax=1&signature=-WBlZGI8J-KDG9Et8VUxftU1LPc=',
+    },
+    {
+        id:8,
+        Name:'Irina Shayk',
+        Time:'2021',
+        Img:'https://imgs.capitalxtra.com/images/243114?crop=16_9&width=660&relax=1&signature=1Qiwrkmdy204v-1i57OU6KJMVOM=',
+    },
+    {
+        id:9,
+        Name:'Julia Fox',
+        Time:'2022',
+        Img:'https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&rect=12%2C0%2C1137%2C1500&poi=%5B303%2C90%5D&w=1168&h=1557&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2022%2F01%2F23%2FKanye-West-Julia-Fox.jpg',
+    },
+    {
+        id:10,
+        Name:'Chaney Jones',
+        Time:'2022',
+        Img:'https://imgs.capitalxtra.com/images/290393?crop=16_9&width=660&relax=1&signature=8JC-mx5M0V1DYXomgzB2o4MIUUM=',
+    },
+]
+
+export default girls
